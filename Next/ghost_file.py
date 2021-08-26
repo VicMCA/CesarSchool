c61105043c72979faa69145a7f83e601d37a28e4
VicMCA (Victor) <victor.mcavalcanti@gmail.com>
2021-08-26 19:49:36 -0300

    shy ghost file

--- Next/ghost_file.py
+++ Next/ghost_file.py
@@ -1,1 +1,3 @@
 print("I'm a ghost file, booooooooooo")
+print("But honestly, I'm kinda shy and it's weird being the only new guy around here.\n"
++"Mind introducing me to some of your friends? 😿") 
