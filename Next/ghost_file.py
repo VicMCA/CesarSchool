1261ebc45e03575945e4afaf3aa4193cf21a21c8
VicMCA (Victor) <victor.mcavalcanti@gmail.com>
2021-08-26 20:03:42 -0300

    ghost is now happy

--- Next/ghost_file.py
+++ Next/ghost_file.py
@@ -1,3 +1,4 @@
 print("I'm a ghost file, booooooooooo")
 print("But honestly, I'm kinda shy and it's weird being the only new guy around here.\n"
-+"Mind introducing me to some of your friends? 😿") 
++"Mind introducing me to some of your friends? 😿")
+print("Now I'm not alone anymore 💖") 
