e3361096e5bb7e2f597b87d97726c83e0aeff160
VicMCA (Victor) <victor.mcavalcanti@gmail.com>
2021-09-02 18:32:09 -0300

    Mais dois exercícios adicionados

---
+++ JavaNext/src/ExercicioJava06MediaAritmetica.java
@@ -0,0 +1,10 @@
+public class ExercicioJava06MediaAritmetica {
+    public static void main(String[] args) throws Exception{
+        double media1 = (7 + 8 + 9) / 3;
+        double media2 = (4 + 5 + 6) / 3;
+
+        System.out.println("Primeira média: " + media1);
+        System.out.println("Segunda média: " + media2);
+        System.out.println("Média geral: " + (media1 + media2) / 2);
+    }
+}
