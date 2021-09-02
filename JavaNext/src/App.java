a2d1da2c88c07612d8e871f5cb7f34da370ba3ef
VicMCA (Victor) <victor.mcavalcanti@gmail.com>
2021-09-02 12:48:18 -0300

    Novos exemplos e exercícios + problema a ser resolvido

--- JavaNext/src/App.java
+++ JavaNext/src/App.java
@@ -14,7 +14,6 @@ public class App {
         System.out.println(total - 30);
 
         System.out.println(full.toString());
-
         String meuNome = "Victor Cavalcanti";
 
         System.out.println(meuNome.toUpperCase());
@@ -63,5 +62,85 @@ public class App {
         short d = (short) a;
 
         System.out.println(a + b + c + d);
+
+        /* Escopo de blocos é importante, representado por chaves.
+        A identação não é obrigatória, mas ajuda com legibilidade
+        e é fortemente recomendada. */
+        
+        {
+            int aa = 01;
+            {
+                int ab = 02;
+                System.out.println(aa + ab);
+            }
+            int ac = 03;
+            System.out.println(aa + ac);
+        }
+
+        /* No exemplo acima, int aa e ac estão fora do escopo de ab.
+        É possível acessar aa dentro do escopo de ab, mas não é possível
+        acessar ab fora do escopo dele */
+
+        int x = 1;
+        if (x > 2) {
+            System.out.println("1 é maior que 2");
+        } else if (x == 2) {
+            System.out.println("1 é igual a 2");
+        } else {
+            System.out.println("A matemática é real");
+        }
+        /* Estrutura básica de if/else/else-if acima. Trabalha com
+        valores verdadeiros e falsos */
+
+        while (x < 10) {
+            x++;
+        }
+        /* Estrutura básica de loop while. Do/While também existe
+        Use while (true) para um loop infinito, e use break  para
+        sair do código */
+
+        x = 0;
+        do {
+            x++;
+        } while (x < 10);
+        /* No Do/While o teste é feito no final*/
+
+        for (int count = 0; count < 10; count++) {
+            System.out.println("contagem: " + count);
+        }
+        /* Exemplo de loop for */
+
+        x = 2;
+        switch (x) {
+            case 1:
+                System.out.println("x = 1");
+                break;
+            case 2:
+                System.out.println("x = 2");
+                break;
+            case 3:
+                System.out.println("x = 3");
+                break;
+            default:
+                System.out.println("x = indefinido");
+                break;
+        }
+        // Exemplo de switch/case, outro exemplo abaixo
+
+        final int CREDITO = 1; // Usamos "final" pra declarar constantes
+        final int DEBITO = 2;
+        int formaPagamento = 1;
+
+        switch (formaPagamento) {
+            case CREDITO:
+                System.out.println("Pagamento em crédito");
+                break;
+            case DEBITO:
+                System.out.println("Pagamento em débito");
+                break;
+            default:
+                System.out.println("Pagamento não realizado");
+                break;
+        }
     }
 }
--- JavaNext/src/ConversorTemperatura.java
+++ JavaNext/src/ConversorTemperatura.java
@@ -1,17 +1,72 @@
+import java.util.Scanner; 
+
 public class ConversorTemperatura {
     public static void main(String[] args) {
         
-        float celsius = 30.0f;
+        int unidade = 0;
+        double temperatura = 0.0;
+        
+        /* Descrição do problema:
+            Estou tendo um loop infinito de prints ao tentar solucionar um problema de vazamento de 
+        memória que o VSCode acusa em um dos objetos de Scanner. O problema também ocorre se eu utilizar
+        instâncias com nomes diferentes entre si.
+            Faço uso de dois loops while(true){try{}catch{}} para evitar que o usuário insira dados
+        inválidos, mas desejo manter o programa solicitando inputs até que o usuário insira algo correto. */
 
-        float fahrenheit = (celsius * 1.8f) + 32f;
-        float kelvin = celsius + 273.15f;
-        float reaumur = celsius * 0.8f;
-        float rankine = (celsius * 1*8f) + 32f + 459.67f;
+        while(true) {
+            try {
+                System.out.println("Qual a unidade de temperatura que deseja usar? ");
+                System.out.println("[1] Celsius [2] Fahrenheit [3] Kelvin [4] Réaumur [5] Rankine");
+                Scanner read = new Scanner(System.in);
+                unidade = read.nextInt();
+                read.close(); // <- Parte problemática
+                /* O VSCode acusa um problema de vazamento de memória se a linha do "read.close()" não
+                estiver presente, mas com ela presente, o código entra em um print infinito ao chegar no
+                próximo while(true) {try{}catch{}} */                
+                break;
+            } catch(Exception e) {
+                System.out.printf("Valor inválido. ");
+            }
+        }
+                
+        while(true) {
+            System.out.printf("Informe a temperatura na unidade informada: ");
+            try {
+                Scanner read = new Scanner(System.in);
+                temperatura = read.nextDouble();
+                read.close(); // <- Parte problemática, idêntica a de cima
+                break;
+            } catch(Exception e) {
+                System.out.printf("Valor inválido. ");
+            }
+        }
+        
+        double kelvin = temperatura;
+        double celsius = kelvin - 273.15;
+        double fahrenheit = (kelvin * 1.8) + 459.67;
+        double reaumur = (kelvin - 273.15) * 0.8;
+        double rankine = kelvin * 1.8;
 
-        System.out.println(celsius + "º celsius são iguais a:");
-        System.out.println(fahrenheit+ "º Fahrenheit");
-        System.out.println(kelvin + "º Kelvin");
-        System.out.println(reaumur+ "º Réaumur");
-        System.out.println(rankine + "º Rankine");
+        // A parte abaixo ainda está incompleta.
+
+        switch (unidade) {
+            case 1:
+                System.out.println(celsius + "º Celsius");
+                break;
+            case 2:
+                System.out.println(fahrenheit+ "º Fahrenheit");
+                break;
+            case 3:
+                System.out.println(kelvin + "º Kelvin");
+                break;
+            case 4:
+                System.out.println(reaumur+ "º Réaumur");
+                break;
+            case 5:
+                System.out.println(rankine + "º Rankine");
+                break;
+            default:
+                break;
+        }   
     }
 }
